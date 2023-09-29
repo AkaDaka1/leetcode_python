17f9fabfc24a6b5320ebe06a27a9237ea96d717f
Danil M <85983884+AkaDaka1@users.noreply.github.com>
2023-09-30 00:27:35 +0300

    Update 9-Palindrome Number.py
    Added new method

--- 9-Palindrome Number.py
+++ 9-Palindrome Number.py
@@ -1,5 +1,6 @@
 # Given an integer x, return true if x is a 
 # palindrome, and false otherwise.
+# Follow up: Could you solve it without converting the integer to a string?
 # ------------------------------------
 class Solution(object):
     def isPalindrome(self, x):
@@ -19,10 +20,36 @@ class Solution(object):
         else:
             return False
 # ------------------------------------
-# Runtime
-# 39ms
+# Runtime 39ms
 # Beats 65.03%of users with Python
-
-# Memory
-# 12.99MB
+# Memory 12.99MB
 # Beats 98.84%of users with Python
+# ------------------------------------
+#
+#
+# Ver 2
+# Using converting the integer to a string
+# ------------------------------------
+class Solution(object):
+    def isPalindrome(self, x):
+        if x>=0 and x<10:
+            return True
+        elif x < 0:
+            return False
+        else:
+            x = str(x)
+            a = len(x)
+            b = 0
+            while a>len(x)//2:
+                if x[a-1]==x[b]:
+                    b+=1
+                    a-=1
+                else:
+                    return False
+            return True
+# ------------------------------------
+# Runtime 31ms
+# Beats 88.85%of users with Python
+# Memory 13.14MB
+# Beats 75.78%of users with Python
+# ------------------------------------
